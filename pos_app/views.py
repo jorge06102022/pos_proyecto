18b7c5cde316001457341dda1e001c70885c280b
CoronelPelotas666 <yupizz69@gmail.com>
2026-05-01 21:07:36 -0500

    jjjjhlkg

--- pos_app/views.py
+++ pos_app/views.py
@@ -845,14 +845,14 @@ def crear_admin(request):
 
     try:
         user, created = User.objects.get_or_create(
-            username="wiliam",
+            username="kelly",
             defaults={
-                "email": "jordav8a@gmail.com",
+                "email": "jordava@gmail.com",
             }
         )
 
         if created:
-            user.set_password("12345678")
+            user.set_password("1234567890")
             user.is_superuser = True
             user.is_staff = True
             user.save()
@@ -875,53 +875,23 @@ from django.shortcuts import render
 import json
 from .models import Egreso
 
-@csrf_exempt
+@login_required
 def egresos_list_create(request):
 
-    # 🔥 GET → LISTA
-    if request.method == 'GET':
-        egresos = Egreso.objects.all().order_by('-fecha')
+    if request.method == 'POST':
+        Egreso.objects.create(
+            nombre=request.POST.get('nombre'),
+            descripcion=request.POST.get('descripcion'),
+            monto=request.POST.get('monto'),
+            metodo_pago=request.POST.get('metodo_pago')
+        )
+        return redirect('egresos_list_create')
 
-        # 👉 Si viene desde navegador → HTML
-        if 'text/html' in request.headers.get('Accept', ''):
-            return render(request, 'egresos/lista.html', {
-                'egresos': egresos
-            })
+    egresos = Egreso.objects.all().order_by('-fecha')
 
-        # 👉 Si viene desde fetch / API → JSON
-        data = [
-            {
-                "id": e.id,
-                "nombre": e.nombre,
-                "descripcion": e.descripcion,
-                "monto": float(e.monto),
-                "metodo_pago": e.metodo_pago,
-                "fecha": e.fecha.strftime('%Y-%m-%d %H:%M')
-            }
-            for e in egresos
-        ]
-
-        return JsonResponse(data, safe=False)
-
-    # 🔥 POST → CREAR
-    elif request.method == 'POST':
-        try:
-            body = json.loads(request.body)
-
-            egreso = Egreso.objects.create(
-                nombre=body.get('nombre'),
-                descripcion=body.get('descripcion'),
-                monto=body.get('monto'),
-                metodo_pago=body.get('metodo_pago', 'efectivo')
-            )
-
-            return JsonResponse({
-                "message": "Egreso creado correctamente",
-                "id": egreso.id
-            }, status=201)
-
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=400)
+    return render(request, 'egresos/lista.html', {
+        'egresos': egresos
+    })
 
 @csrf_exempt
 def egreso_detail(request, pk):
